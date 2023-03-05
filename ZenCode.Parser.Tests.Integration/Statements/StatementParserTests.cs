6747489061247a5c8084c8ba9ef077ab26e20774
Thomas Mathers <thomas.mathers@ideanotion.net>
2023-03-04 22:02:35 -0500

    added more tests

--- ZenCode.Parser.Tests.Integration/Statements/StatementParserTests.cs
+++ ZenCode.Parser.Tests.Integration/Statements/StatementParserTests.cs
@@ -864,7 +864,173 @@ public class StatementParserTests
         // Assert
         Assert.Equal(expectedStatement, actualStatement);
     }
+    
+    [Fact]
+    public void Parse_DeclareVariableAndAssignBinaryExpression_ReturnsVariableDeclarationStatement()
+    {
+        // Arrange
+        var tokenStream = new TokenStream(new[]
+        {
+            new Token(TokenType.Var),
+            new Token(TokenType.Identifier),
+            new Token(TokenType.Assignment),
+            new Token(TokenType.IntegerLiteral),
+            new Token(TokenType.Addition),
+            new Token(TokenType.IntegerLiteral)
+        });
+        
+        var identifier = new Token(TokenType.Identifier);
 
+        var expression = new BinaryExpression(
+            new ConstantExpression(new Token(TokenType.IntegerLiteral)),
+            new Token(TokenType.Addition),
+            new ConstantExpression(new Token(TokenType.IntegerLiteral)));
+
+        var expectedStatement = new VariableDeclarationStatement(identifier, expression);
+
+        // Act
+        var actualStatement = _sut.ParseStatement(tokenStream);
+
+        // Assert
+        Assert.Equal(expectedStatement, actualStatement);
+    }
+
+    [Theory]
+    [InlineData(TokenType.BooleanLiteral)]
+    [InlineData(TokenType.IntegerLiteral)]
+    [InlineData(TokenType.FloatLiteral)]
+    [InlineData(TokenType.StringLiteral)]
+    public void Parse_DeclareVariableAndAssignLiteral_ReturnsVariableDeclarationStatement(TokenType tokenType)
+    {
+        // Arrange
+        var tokenStream = new TokenStream(new[]
+        {
+            new Token(TokenType.Var),
+            new Token(TokenType.Identifier),
+            new Token(TokenType.Assignment),
+            new Token(tokenType)
+        });
+        
+        var identifier = new Token(TokenType.Identifier);
+
+        var expression = new ConstantExpression(new Token(tokenType));
+
+        var expectedStatement = new VariableDeclarationStatement(identifier, expression);
+
+        // Act
+        var actualStatement = _sut.ParseStatement(tokenStream);
+
+        // Assert
+        Assert.Equal(expectedStatement, actualStatement);
+    }
+
+    [Fact]
+    public void Parse_DeclareVariableAndAssignFunctionCallExpression_ReturnsVariableDeclarationStatement()
+    {
+        // Arrange
+        var tokenStream = new TokenStream(new[]
+        {
+            new Token(TokenType.Var),
+            new Token(TokenType.Identifier),
+            new Token(TokenType.Assignment),
+            new Token(TokenType.Identifier),
+            new Token(TokenType.LeftParenthesis),
+            new Token(TokenType.RightParenthesis)
+        });
+
+        var identifier = new Token(TokenType.Identifier);
+
+        var expression = new FunctionCallExpression(new VariableReferenceExpression(new Token(TokenType.Identifier)));
+
+        var expectedStatement = new VariableDeclarationStatement(identifier, expression);
+
+        // Act
+        var actualStatement = _sut.ParseStatement(tokenStream);
+
+        // Assert
+        Assert.Equal(expectedStatement, actualStatement);
+    }
+
+    [Fact]
+    public void Parse_DeclareVariableAndAssignParenthesisExpression_ReturnsVariableDeclarationStatement()
+    {
+        // Arrange
+        var tokenStream = new TokenStream(new[]
+        {
+            new Token(TokenType.Var),
+            new Token(TokenType.Identifier),
+            new Token(TokenType.Assignment),
+            new Token(TokenType.LeftParenthesis),
+            new Token(TokenType.StringLiteral),
+            new Token(TokenType.RightParenthesis)
+        });
+        
+        var identifier = new Token(TokenType.Identifier);
+
+        var expression = new ConstantExpression(new Token(TokenType.StringLiteral));
+
+        var expectedStatement = new VariableDeclarationStatement(identifier, expression);
+
+        // Act
+        var actualStatement = _sut.ParseStatement(tokenStream);
+
+        // Assert
+        Assert.Equal(expectedStatement, actualStatement);
+    }
+
+    [Fact]
+    public void Parse_DeclareVariableAndAssignUnaryExpression_ReturnsVariableDeclarationStatement()
+    {
+        // Arrange
+        var tokenStream = new TokenStream(new[]
+        {
+            new Token(TokenType.Var),
+            new Token(TokenType.Identifier),
+            new Token(TokenType.Assignment),
+            new Token(TokenType.Subtraction),
+            new Token(TokenType.FloatLiteral)
+        });
+
+        var identifier = new Token(TokenType.Identifier);
+        
+        var expression = new UnaryExpression(
+            new Token(TokenType.Subtraction),
+            new ConstantExpression(new Token(TokenType.FloatLiteral)));
+
+        var expectedStatement = new VariableDeclarationStatement(identifier, expression);
+
+        // Act
+        var actualStatement = _sut.ParseStatement(tokenStream);
+
+        // Assert
+        Assert.Equal(expectedStatement, actualStatement);
+    }
+
+    [Fact]
+    public void Parse_DeclareVariableAndAssignVariableReferenceExpression_ReturnsVariableDeclarationStatement()
+    {
+        // Arrange
+        var tokenStream = new TokenStream(new[]
+        {
+            new Token(TokenType.Var),
+            new Token(TokenType.Identifier),
+            new Token(TokenType.Assignment),
+            new Token(TokenType.Identifier)
+        });
+        
+        var identifier = new Token(TokenType.Identifier);
+
+        var expression = new VariableReferenceExpression(new Token(TokenType.Identifier));
+        
+        var expectedStatement = new VariableDeclarationStatement(identifier, expression);
+
+        // Act
+        var actualStatement = _sut.ParseStatement(tokenStream);
+
+        // Assert
+        Assert.Equal(expectedStatement, actualStatement);
+    }
+    
     [Fact]
     public void Parse_WhileBinaryExpressionAssignmentStatement_ReturnsWhileStatement()
     {
